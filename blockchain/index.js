9f4a46be2c139ba2e556bbd3bdd9eb61b6ce7ecb
sselea <sselea11@gmail.com>
2021-10-23 08:14:51 -0400

    added wallets and transactions

--- blockchain/index.js
+++ blockchain/index.js
@@ -1,11 +1,11 @@
 const Block = require('./block');
-const cryptoHash = require('./crypto-hash');
+const cryptoHash = require('../util/crypto-hash');
 
 class Blockchain {
     constructor() {
         this.chain = [Block.genesis()]
     }
- 
+
     addBlock({ data }) {
         const newBlock = Block.mineBlock({
             lastBlock: this.chain[this.chain.length - 1],
@@ -35,12 +35,15 @@ class Blockchain {
         for (let i = 1; i < chain.length; i++) {
             const { timestamp, lastHash, hash, data, nonce, difficulty } = chain[i];
             const actualLastHash = chain[i - 1].hash;
+            const lastDifficulty = chain[i - 1].difficulty;
 
             if (lastHash !== actualLastHash) return false;
 
             const validatedHash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
 
             if (hash !== validatedHash) return false;
+
+            if (Math.abs(lastDifficulty - difficulty > 1)) return false;
         }
         return true;
     }
